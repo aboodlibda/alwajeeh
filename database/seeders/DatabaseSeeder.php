20e3ad9997b038b491cced6246d82892dd95b2cb
aboodlibda <aboodlibda@gmail.com>
2024-08-30 16:19:03 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-//        $this->call(AdminSeeder::class);
+       $this->call(AdminSeeder::class);
         $this->call(SectionSeeder::class);
 //        User::factory()->create([
 //            'name' => 'Test User',
