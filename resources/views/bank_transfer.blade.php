36e374bea0b33983ccd35ed589398882099ae906
aboodlibda <aboodlibda@gmail.com>
2024-08-30 18:10:53 +0300

    Update bank_transfer.blade.php

--- resources/views/bank_transfer.blade.php
+++ resources/views/bank_transfer.blade.php
@@ -23,8 +23,8 @@
                         </p>
                     </div>
                     <div class="text-center mt-2 mb-4">
-                        <p class="fs-6 text-secondary">بنك الراجحي - IBANPAL1684351645614651531</p>
-                        <p class="fs-6 text-secondary"> بنك الأهلي - RAJSAR54518616546515686651</p>
+                        <p class="fs-6 text-secondary">IBANPAL1684351645614651531</p>
+                        <p class="fs-6 text-secondary">RAJSAR54518616546515686651</p>
                     </div>
                     <div class="text-center my-4">
                         <p class="fs-6 text-secondary" style="    color: #f00 !important;">
@@ -42,7 +42,7 @@
                         </p>
                     </div>
                     <div class="text-center my-3">
-                        <a href="https://wa.me/966598692675" class="btn btn-outline-success w-100">
+                        <a href="https://wa.me/9660591829672" class="btn btn-outline-success w-100">
                             <i class="fas fa-headset fa-fw"></i>
                             خدمة العملاء
                         </a>
